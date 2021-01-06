376ed30f592e776554e7f0b8798b0fcd7123ee44
Jonas Hughes Larsen <jhularsen@gmail.com>
2021-01-06 21:12:08 +0100

    tidy models

--- tidymodels.R
+++ tidymodels.R
@@ -5,6 +5,128 @@ penguin_df <- penguins %>%
   filter(!is.na(sex)) %>%
   select(-year, island)
 
+glimpse(penguin_df)
+skim(penguin_df)
+
+
+# Data splitting ----------------------------------------------------------
+set.seed(123)
 penguin_split <- initial_split(penguin_df)
 penguin_split
+
+penguin_train <- training(penguin_split)
+penguin_test <- testing(penguin_split)
+
+cv_folds <- vfold_cv(penguin_train)
+
+
+# Random Forest -----------------------------------------------------------
+library(usemodels)
+use_ranger(sex~., data = penguin_df) #gives a template for modeling
+
+ranger_recipe <- 
+  recipe(sex ~., data = penguin_train) %>%
+  step_corr(all_numeric()) %>%
+  step_center(all_numeric()) %>%
+  step_scale(all_numeric())
+
+ranger_spec <- 
+  rand_forest(mtry = tune(), min_n = tune(), trees = 1000) %>% 
+  set_mode("classification") %>% 
+  set_engine("ranger") 
+
+ranger_workflow <- 
+  workflow() %>% 
+  add_recipe(ranger_recipe) %>% 
+  add_model(ranger_spec) 
+
+set.seed(48276)
+ranger_tune <-
+  tune_grid(ranger_workflow, resamples = cv_folds, grid = crossing(mtry = 1:3, min_n = c(5, 10)))
+
+# Evaluating 
+ranger_tune %>% 
+  collect_metrics()
+
+ranger_tune %>%
+  select_best(metric = "accuracy")
+
+autoplot(ranger_tune)
+
+rf_param <- 
+  tibble(
+    mtry = 2,
+    min_n = 5)
+
+final_ranger_wflow <- 
+  ranger_workflow %>% 
+  finalize_workflow(rf_param)
+
+final_ranger_wflow
+
+ranger_fit <-
+  final_ranger_wflow %>% 
+  last_fit(penguin_split)
+
+ranger_fit 
+
+ranger_fit %>% 
+  collect_metrics()
+
+ranger_fit %>% 
+  pull(.predictions)
+
+ranger_fit %>% 
+  collect_predictions() %>% 
+  conf_mat(truth = sex, estimate = .pred_class)
+#only 5 penguins are predicted wrong
+
+#final model using all data
+final_ranger_fit <-
+  final_ranger_wflow %>% 
+  fit(penguin_df)
+
+# Logistic Regression -----------------------------------------------------
+logistic_recipe <- 
+  recipe(sex ~., data = penguin_train) %>%
+  step_corr(all_numeric()) %>%
+  step_center(all_numeric()) %>%
+  step_scale(all_numeric())
+
+logistic_spec <- 
+  logistic_reg() %>% 
+  set_mode("classification") %>% 
+  set_engine("glm") 
+
+logistic_workflow <- 
+  workflow() %>% 
+  add_recipe(logistic_recipe) %>% 
+  add_model(logistic_spec) %>%
+  finalize_workflow()
+
+# Evaluating
+logistic_fit <-
+  logistic_workflow %>% 
+  last_fit(penguin_split)
+
+logistic_fit 
+
+logistic_fit %>% 
+  collect_metrics()
+
+logistic_fit %>% 
+  pull(.predictions)
+
+logistic_fit %>% 
+  collect_predictions() %>% 
+  conf_mat(truth = sex, estimate = .pred_class)
+#only 4 penguins are predicted wrong
+
+#final model using all data
+final_logistic_fit <-
+  logistic_workflow %>% 
+  fit(penguin_df)
+
+final_logistic_fit
+
 
