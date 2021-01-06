3533254f78b7d9c43a3a0c067babc8b68eee1d5d
Jonas Hughes Larsen <jhularsen@gmail.com>
2021-01-06 18:48:52 +0100

    new files

---
+++ tidymodels.R
@@ -0,0 +1,8 @@
+# Modeling with tidy models with the aim of predicting penguin sex (male or female)
+
+# Making a new df with the relevant variables 
+penguin_df <- penguins %>%
+  filter(!is.na(sex)) %>%
+  select(-year, island)
+
+penguin_split <- initial_split(penguin_df)
